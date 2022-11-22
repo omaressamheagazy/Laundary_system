e57b68b6bcc85197122d653d8e13293d71fedd2e
omar_essam1 <68665060+omaressamheagazy@users.noreply.github.com>
2022-11-23 07:29:50 +0800

    edit the driver home page name

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -26,7 +26,7 @@ class HomeController extends Controller
         return view('home');
     }
     public function driverHome() {
-        return view("driver.home");
+        return view("Driver.home");
     }
 }
 
