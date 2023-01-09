c8cab20f26445c9dcc3c476e797094bacec59834
elprinceomaressam@gmail.com <elprinceomaressam@gmail.com>
2023-01-10 03:27:33 +0800

    send request every one second

--- src/public/style/assets/js/track.js
+++ src/public/style/assets/js/track.js
@@ -1,53 +1,84 @@
 const successCallback = (position) => {
     console.log(position);
-    var driver_id = $('#driverID').val() ;// driver id
-    var userId = $('#userId').val();
-    var orderId = $('#orderId').val();
-    let url = $('#trackURL').val();
+    var driver_id = $("#driverID").val(); // driver id
+    var userId = $("#userId").val();
+    var orderId = $("#orderId").val();
+    let url = $("#trackURL").val();
     $.ajax({
         headers: {
             "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
         },
-        url: '/driver/order/real',
-        data: { lat: position.coords.latitude, long: position.coords.longitude, userId: userId, orderId: orderId },
+        url: "/driver/order/real",
+        data: {
+            lat: position.coords.latitude,
+            long: position.coords.longitude,
+            userId: userId,
+            orderId: orderId,
+        },
         type: "POST",
-        dataType: "json"
+        dataType: "json",
     });
 };
 const errorCallback = (error) => {
     getPositionErrorMessage(error.code);
 };
-const getPositionErrorMessage = code => {
-  switch (code) {
-    case 1:
-      alert( 'Permission denied, please make sure that you enable location, and reload the page.');
-      break;
-    case 2:
-      alert ('Position unavailable.');
-      break;
-    case 3:
-      alert( 'Timeout reached.');
-      break;
-    default:
-      return null;
-  }
-}
-
-options = {
-  enableHighAccuracy: false,
-  timeout: 10000,
-  maximumAge: 0
+const getPositionErrorMessage = (code) => {
+    switch (code) {
+        case 1:
+            alert(
+                "Permission denied, please make sure that you enable location, and reload the page."
+            );
+            break;
+        case 2:
+            alert("Position unavailable.");
+            break;
+        case 3:
+            alert("Timeout reached.");
+            break;
+        default:
+            return null;
+    }
 };
 
-let orderStatus   =  parseInt( $('#orderStatus').val() ) ; // get order status id
-let pickUpStatus  =  parseInt(  $('#pickUpStatus').val() );
-let deliverStatus =  parseInt( $('#deliverStatus').val() );
+options = {
+    enableHighAccuracy: false,
+    timeout: 10000,
+    maximumAge: 0,
+};
 
-if(orderStatus == pickUpStatus || orderStatus == deliverStatus) { // activate the live location, only if the driver is picking up or deliver user laundry
-    if('geolocation' in navigator) {
-      navigator.geolocation.watchPosition(successCallback, errorCallback, options);
+let orderStatus = parseInt($("#orderStatus").val()); // get order status id
+let pickUpStatus = parseInt($("#pickUpStatus").val());
+let deliverStatus = parseInt($("#deliverStatus").val());
+
+if (orderStatus == pickUpStatus || orderStatus == deliverStatus) {
+    // activate the live location, only if the driver is picking up or deliver user laundry
+    if ("geolocation" in navigator) {
+        function mockGetCurrentPosition() {
+            return navigator.geolocation.getCurrentPosition(
+                successCallback,
+                errorCallback
+            );
+        }
+
+        async function getPosition() {
+            await mockGetCurrentPosition();
+            setTimeout(() => getPosition(), 5000);
+        }
+
+        getPosition();
+        // navigator.geolocation.watchPosition(successCallback, errorCallback, options);
     } else {
-      alert('Geolocation is not supported by your browser.');
+        alert("Geolocation is not supported by your browser.");
     }
 }
 
+// function mockGetCurrentPosition () {
+//   return navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+// }
+
+// async function getPosition () {
+//   await mockGetCurrentPosition();
+//   setTimeout(()=> getPosition(), 5000);
+// }
+
+// getPosition();
